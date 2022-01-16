75172f2c08b9c054aec9d02bfac3736e5a081175
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-01-16 20:41:58 +0100

    Added dolar sign removal to extract_dataset.py

--- extract_dataset.py
+++ extract_dataset.py
@@ -1,13 +1,24 @@
-DIRECTORY = 'dataset/'
-DATASET_NAME = 'dow_jones_index'
+DIRECTORY = "dataset/"
+DATASET_NAME = "dow_jones_index"
+FORMAT_END = "_formatted"
 
 import zipfile, os
 
 dir_list = os.listdir(DIRECTORY)
-# check if dataset folder have expracted files
-if not DATASET_NAME + '.data' in dir_list or not DATASET_NAME + '.names' in dir_list:
+# check if dataset folder have extracted files
+if (
+    not DATASET_NAME + ".data" in dir_list
+    or not DATASET_NAME + ".names" in dir_list
+):
     # if not, extract all
-    with zipfile.ZipFile(DIRECTORY + DATASET_NAME + '.zip', 'r') as zip:
+    with zipfile.ZipFile(DIRECTORY + DATASET_NAME + ".zip", "r") as zip:
         zip.extractall(DIRECTORY)
 
-# find paragraph related to names
+# remove all dolar sign from data if it's not done yet
+if not DATASET_NAME + FORMAT_END + ".data" in dir_list:
+    with open(DIRECTORY + DATASET_NAME + ".data", "r") as in_f, open(
+        DIRECTORY + DATASET_NAME + FORMAT_END + ".data", "w+"
+    ) as out_f:
+        # remove all dolar sign from each line
+        lines = map(lambda x: x.replace("$", ""), in_f.readlines())
+        out_f.writelines(lines)
